4f815f963821634c6ffc32507284cbe361b30fbf
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-01-04 18:03:11 +0100

    Update place-amenity

--- models/place.py
+++ models/place.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Table, Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
+from models import storage_type
+
+
+place_amenities = Table(
+    'place_amenity',
+    Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'),
+           primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+           primary_key=True, nullable=False)
+)
 
 
 class Place(BaseModel, Base):
@@ -21,6 +33,34 @@ class Place(BaseModel, Base):
     reviews = relationship(
             "Review", backref="place", cascade="all, delete-orphan"
             )
+
+    if storage_type == "db":
+        amenities = relationship(
+            'Amenity',
+            secondary='place_amenity',
+            viewonly=False,
+            back_populates='place_amenities'
+        )
+    else:
+        # For FileStorage, use a list to story Amenity IDs
+        @property
+        def amenities(self):
+            """Getter for amenities"""
+            from models import storage
+            amenity_list = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.get(Amenity, amenity_id)
+                if amenity:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter for amenity id"""
+            from models.amenity import Amenity
+            if isinstance(obj, Amenity):
+                if obj.id not in self.amenity_ids:
+                    self.amenity_ids.append(obj.id)
 
     @property
     def reviews(self):
