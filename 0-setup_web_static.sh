aaed7c55fc12f79365d1b45529c91bf1dea95a49
fikayosalu <salufikayo@gmail.com>
2025-01-27 23:30:02 +0100

    Nginx config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,17 +20,13 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-nginx_config="\
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-    }"
-
-if ! grep -q "hbnb_static" /etc/nginx/sites-available/default; then
-    sudo sed -i "/server_name _;/a $nginx_config" /etc/nginx/sites-available/default
-fi
+sudo sed -i '54a\
+	location ^~ /hbnb_static{\
+		alias /data/web_static/current/\
+		try_files $uri $uri/ =404;\
+	}' /etc/nginx/sites-available/default
 
 # Restart Nginx to apply changes
 sudo service nginx restart
 
 echo "Setup complete! Web static content is now available at /hbnb_static/"
-
