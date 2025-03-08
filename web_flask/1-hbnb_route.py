1bd1f4237e84fcc130721975bd01356a80e53069
fikayosalu <salufikayo@gmail.com>
2025-03-08 18:43:42 +0100

    flask route 2

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""
+Starts a flask application that displays
+for certain routes
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
